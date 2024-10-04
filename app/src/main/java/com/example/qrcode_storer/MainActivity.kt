ffcd2af565935d7a90472a151425e4cd1ed0deaa
gl.palma <gl.palma@samsung.com>
2024-10-04 16:08:27 -0300

    Creation of bottom navigation bar

--- app/src/main/java/com/example/qrcode_storer/MainActivity.kt
+++ app/src/main/java/com/example/qrcode_storer/MainActivity.kt
@@ -1,19 +1,46 @@
 package com.example.qrcode_storer
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.outlined.Favorite
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material3.Badge
+import androidx.compose.material3.BadgedBox
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.content.res.ResourcesCompat
+import com.example.qrcode_storer.ui.BottomNavigationTheme
 import com.example.qrcode_storer.ui.MainScreen
 import com.example.qrcode_storer.ui.theme.QrcodestorerTheme
+
+data class BottomNavigationItem(
+    val title: String,
+    val selectedIcon: ImageVector,
+    val unselectedIcon: ImageVector,
+    val hasNews: Boolean,
+    val badgeCount: Int? = null
+)
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,12 +48,80 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             QrcodestorerTheme {
-                Surface (
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    MainScreen()
+                BottomNavigationTheme {
+                    val items = listOf(
+                        BottomNavigationItem(
+                            title = "Scanner",
+                            selectedIcon = Icons.Filled.Home,
+                            unselectedIcon = Icons.Outlined.Home,
+                            hasNews = false,
+                        ),
+                        BottomNavigationItem(
+                            title = "Favorites",
+                            selectedIcon = Icons.Filled.Favorite,
+                            unselectedIcon = Icons.Outlined.Favorite,
+                            hasNews = false,
+                        ),
+                    )
+                    var selectedItemIndex by rememberSaveable {
+                        mutableIntStateOf(0)
+                    }
+                    Surface(
+                        modifier = Modifier.fillMaxSize(),
+                        color = MaterialTheme.colorScheme.background
+                    ) {
+                        Scaffold(
+                            bottomBar = {
+                                NavigationBar {
+                                    items.forEachIndexed { index, item ->
+                                        NavigationBarItem(
+                                            selected = selectedItemIndex == index,
+                                            onClick = {
+                                                selectedItemIndex = index
+                                                // navController.navigate(item.title)
+                                                // TODO: implement navigation
+                                            },
+                                            label = {
+                                                Text(text = item.title)
+                                            },
+                                            alwaysShowLabel = false,
+                                            icon = {
+                                                BadgedBox(
+                                                    badge = {
+                                                        if (item.badgeCount != null) {
+                                                            Badge {
+                                                                Text(text = item.badgeCount.toString())
+                                                            }
+                                                        } else if (item.hasNews) {
+                                                            Badge()
+                                                        }
+                                                    }
+                                                ) {
+                                                    Icon(
+                                                        imageVector = if (index == selectedItemIndex) {
+                                                            item.selectedIcon
+                                                        } else item.unselectedIcon,
+                                                        contentDescription = item.title
+                                                    )
+                                                }
+                                            }
+                                        )
+                                    }
+                                }
+                            }
+                        ) { padding ->
+
+                            Surface(
+                                modifier = Modifier.fillMaxSize().padding(padding),
+                                color = MaterialTheme.colorScheme.background
+                            ) {
+                                MainScreen()
+                            }
+
+                        }
+                    }
                 }
+
             }
         }
     }
@@ -35,7 +130,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun AppTitle() {
     Text(
-        text = "QRCode store",
+        text = "MLKit QRCode scanner",
         style = MaterialTheme.typography.bodyLarge
     )
 }
---
+++ app/src/main/java/com/example/qrcode_storer/ui/favorites/FavoritesScreen.kt
@@ -0,0 +1,9 @@
+package com.example.qrcode_storer.ui.favorites
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun FavoritesScreen() {
+    Text(text = "oie vai vir coisa ainda")
+}
